3cd1a7fd799aa01542a10aad77f69cd6f295f271
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-01-12 11:12:23 +0530

    Initial Commit

---
+++ SwiftuiCoins/SwiftuiCoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCoinsApp.swift
+//  SwiftuiCoins
+//
+//  Created by Bhavik Bhatt on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
